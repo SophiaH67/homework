655102a82023d6c58e7a8ea69e745bef932aedd2
marnixah <marnix@hagehoeve.com>
2020-08-21 22:40:42 +0200

    Removed status

--- www/js/main.js
+++ www/js/main.js
@@ -6,15 +6,15 @@
  * @param {Number} date 
  * @param {String} link 
  */
-function generateCard(title, tasks, date, status, id, link) {
+function generateCard(title, tasks, date, id, link) {
     let time = new Date(date);
-    return `<div class="card" style="width: 18rem;">
+    return `<div class="card" style="width: 18rem;" id="">
                 <img class="card-img-top" src="${title}.png" onerror="this.src='error.png'" alt="Image of ${title}">
                 <div class="card-body">
                     <h5 class="card-title">${title}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">${('0' + time.getMonth()).slice(-2)}/${('0' + time.getDate()).slice(-2)} ${('0' + time.getHours()).slice(-2)}:${('0' + time.getMinutes()).slice(-2)}</h6>
 
-                    <a ${status == 0 ? "href='" + link +"' " : "onClick=' notify(\"Homework hasn't been added yet\", \"error\"); '"} class="btn btn-${ status == 0 ? "success" : "danger"}">${tasks}</a>
+                    <a ${link.length < 5 ? "href='" + link +"' " : "onClick=' notify(\"Homework hasn't been added yet\", \"error\"); '"} class="btn btn-${ link.length == 0 ? "success" : "danger"}">${tasks}</a>
                 </div>
             </div>`;
 }
