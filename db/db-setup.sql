a7e06ec14fd1b820c40d92cf2b5cd37b8a082726
marnixah <marnix@hagehoeve.com>
2020-08-21 18:07:21 +0200

    Added a db-setup.sql to automate setting up database

---
+++ db/db-setup.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE homework;
+
+CREATE TABLE entries (
+    ID int NOT NULL AUTO_INCREMENT,
+    title varchar(255),
+    tasks varchar(255),
+    date BIGINT,
+    status SMALLINT,
+    link varchar(255)
+);
