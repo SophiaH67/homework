79999704f08a50c6e8471bf20cd35bfd0b3ace1f
marnixah <marnix@hagehoeve.com>
2020-08-21 21:17:11 +0200

    Closing the mysql connection after it's used

--- api/editHomework.js
+++ api/editHomework.js
@@ -2,4 +2,5 @@
 exports.run = function (url) {
     const connection = require('./lib/sql.js');
     connection.query("SELECT * FROM entries WHERE date < " + new Date().getTime());
+    connection.destroy();
 }
