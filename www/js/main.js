44081c7072b8d9cfd5b0d8f65313f7dafb0f5a32
marnixah <marnix@hagehoeve.com>
2020-08-21 22:10:43 +0200

    Moved from button to anchor tag

--- www/js/main.js
+++ www/js/main.js
@@ -14,7 +14,7 @@ function generateCard(title, tasks, date, status, id, link) {
                     <h5 class="card-title">${title}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">${('0' + time.getMonth()).slice(-2)}/${('0' + time.getDate()).slice(-2)} ${('0' + time.getHours()).slice(-2)}:${('0' + time.getMinutes()).slice(-2)}</h6>
 
-                    <button ${status == 0 ? "href='" + link +"' " : "onClick=' notify(\"Homework hasn't been added yet\", \"error\"); '"} class="btn btn-${ status == 0 ? "success" : "danger"}">${tasks}</button>
+                    <a ${status == 0 ? "href='" + link +"' " : "onClick=' notify(\"Homework hasn't been added yet\", \"error\"); '"} class="btn btn-${ status == 0 ? "success" : "danger"}">${tasks}</a>
                 </div>
             </div>`;
 }
