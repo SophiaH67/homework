9453795c87f51be881a973a6f5cbb3eb0e4c9401
marnixah <marnix@hagehoeve.com>
2020-08-21 20:59:24 +0200

    Fixed table creation script

--- db/setup/01 - create table.sql
+++ db/setup/01 - create table.sql
@@ -1,8 +1,9 @@
-CREATE TABLE entries (
+CREATE TABLE homework.entries (
     ID int NOT NULL AUTO_INCREMENT,
     title varchar(255),
     tasks varchar(255),
     date BIGINT,
     status SMALLINT,
-    link varchar(255)
+    link varchar(255),
+    PRIMARY KEY( ID)
 );
