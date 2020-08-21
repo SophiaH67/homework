038250c11aa8ebfc56ab7bbdae067568a1d8fcdd
marnixah <marnix@hagehoeve.com>
2020-08-21 21:34:50 +0200

    Updated API handler to handle get,post,put,delete

--- server.js
+++ server.js
@@ -8,14 +8,19 @@ app.use(express.static('www/css'));
 app.use(express.static('www/img'));
 app.use(express.static('www/js'));
 
-app.get('/api/*', function (req, res) {
+app.get('/api/*', apiHandler);
+app.post('/api/*', apiHandler);
+app.put('/api/*', apiHandler);
+app.delete('/api/*', apiHandler);
+
+function apiHandler(req, res) {
     let api = req.url.split('/');
     api.shift();
     api.shift();
     api = api[0];
     const result = require(`./api/${api}.js`);
     res.send(result.run(req));
-});
+}
 
 app.listen(port, () => {
     console.log(`Homework app listening at http://localhost:${port}`)
