f3662641f9a21a59507bf88bc19c5620ae95e4b2
marnixah <marnix@hagehoeve.com>
2020-08-21 18:49:26 +0200

    Added API handler

---
+++ api/addHomework.js
@@ -0,0 +1,1 @@
+exports.result = 'test';
--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(express.static('www/css'));
 app.use(express.static('www/img'));
 app.use(express.static('www/js'));
 
+app.get('/api/*', function (req, res) {
+    let api = req.url.split('/');
+    api.shift();
+    api.shift();
+    api = api[0];
+    const result = require(`./api/${api}.js`);
+    console.log(result);
+    res.send(result);
+});
+
 app.listen(port, () => {
     console.log(`Homework app listening at http://localhost:${port}`)
 })
