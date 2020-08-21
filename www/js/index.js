bb68c5e78cfa47a8ae6e34a8ca56652324363be6
marnixah <marnix@hagehoeve.com>
2020-08-21 22:39:31 +0200

    Removed status

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,9 @@
 function init() {
-    appendCard(generateCard("title", "tasks", new Date(), 0, "1", "google.com"));
-    appendCard(generateCard("title", "tasks", new Date(), 0, "1", "google.com"));
-    appendCard(generateCard("title", "tasks", new Date(), 0, "1", "google.com"));
-    appendCard(generateCard("title", "tasks", new Date(), 0, "1", "google.com"));
-    appendCard(generateCard("title", "tasks", new Date(), 0, "1", "google.com"));
+    appendCard(generateCard("title", "tasks", new Date(),  "1", "google.com"));
+    appendCard(generateCard("title", "tasks", new Date(),  "1", "google.com"));
+    appendCard(generateCard("title", "tasks", new Date(),  "1", "google.com"));
+    appendCard(generateCard("title", "tasks", new Date(),  "1", "google.com"));
+    appendCard(generateCard("title", "tasks", new Date(),  "1", "google.com"));
 
 }
 
