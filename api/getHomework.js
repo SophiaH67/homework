484fe700e62e04119277c4b401899589e5bbe82f
marnixah <marnix@hagehoeve.com>
2020-08-21 19:33:54 +0200

    Changed query in getHomework

--- api/getHomework.js
+++ api/getHomework.js
@@ -1,8 +1,10 @@
 const mysql = require('sync-mysql');
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('./config.json'));
-const connection = new MySql({
+const connection = new mysql({
     host: config.mysql.host,
     user: config.mysql.username,
     password: config.mysql.password
 });
+
+exports.result = connection.query("SELECT * FROM homework WHERE date < " + new Date().getTime());
