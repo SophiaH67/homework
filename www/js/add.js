15a19521bb4dfc055748ef5addb7fef0fdf979e1
marnixah <marnix@hagehoeve.com>
2020-08-22 11:25:12 +0200

    Added save function in add.js

--- www/js/add.js
+++ www/js/add.js
@@ -1,3 +1,31 @@
 function init() {
-    appendCard(generateEditableCard("title", "tasks", new Date(),  "1", "google.com"));
+    appendCard(generateEditableCard("title", "tasks", new Date(), "1", "google.com"));
+}
+
+function save(id) {
+    const title = document.getElementById('title-' + id).value;
+    const tasks = document.getElementById('tasks-' + id).value;
+    const dateArr = document.getElementById('time-' + id).value.split(" ");
+    // Dates and times (yay)
+    const year = dateArr[0];
+    const month = dateArr[1] - 1;
+    const day = dateArr[2];
+
+    const timeArr = dateArr[3].split(":");
+
+    const hour = timeArr[0];
+    const minute = timeArr[1];
+    // Years, months, days, hours, minutes, seconds
+    const date = new Date(year, month, day, hour, minute, 00);
+    
+    let data = {};
+
+    data.title = title;
+    data.tasks = tasks;
+    data.time = date;
+    $.ajax({
+        type: "POST",
+        url: '/api/addHomework',
+        data: data
+    });
 }
