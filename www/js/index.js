dd35a9576de7478881f9a8f6281788a9bd45f98f
marnixah <marnix@hagehoeve.com>
2020-08-22 12:31:34 +0200

    Fixed init function

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,10 @@
 function init() {
-    function getHomework() {
-        $.getJSON("/api/getHomework", function (data) {
-            var items = [];
-            $.each(data, function (key, val) {
-                // Object { ID: 1, title: "title", tasks: "tasks", date: 1595670780000, link: "null" }
-                appendCard(generateCard(val.title, val.tasks, val.date,  val.ID, val.link));
-            });
-            
+    $.getJSON("/api/getHomework", function (data) {
+        var items = [];
+        $.each(data, function (key, val) {
+            // Object { ID: 1, title: "title", tasks: "tasks", date: 1595670780000, link: "null" }
+            appendCard(generateCard(val.title, val.tasks, val.date, val.ID, val.link));
         });
-    }
+    });
 }
 
