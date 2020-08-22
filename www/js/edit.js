4ae56d969b89c19160392b719bc43956cdf7114e
marnixah <marnix@hagehoeve.com>
2020-08-22 14:04:10 +0200

    Basic editing in edit.html/js

--- www/js/edit.js
+++ www/js/edit.js
@@ -1,18 +1,68 @@
 function add(title, tasks, date, id, link) {
     const tbody = document.getElementById('table-container');
-    let element = document.createElement('td');
+    let element = document.createElement('tr');
+    let time = new Date(date);
     element.innerHTML = `
     <tr>
       <th scope="row">${id}</th>
-      <td>${title}</td>
-      <td>${tasks}</td>
-      <td>${link}</td>
-      <td>${new Date(date).toLocaleString()}</td>
+      <td id="title-${id}">${title}</td>
+      <td id="tasks-${id}">${tasks}</td>
+      <td id="link-${id}">${link}</td>
+      <td id="time-${id}">${time.getFullYear()} ${('0'+time.getMonth()).slice(-2)} ${('0'+time.getDate()).slice(-2)} ${time.getHours()}:${time.getMinutes()}</td>
+      <td id="actions-${id}">
+        <button id="edit-${id}" class="btn btn-primary" onclick="edit(this.parentElement.parentElement.id)">Edit</button>
+        <button class="btn btn-danger">Delete</button>
+      </td>
     </tr>
-    `; 
+    `;
+    element.id = id;
     tbody.appendChild(element);
 }
 
 function init() {
-    
+    window.editing = false;
+    $.getJSON("/api/getHomework", function (data) {
+        $.each(data, function (key, val) {
+            // Object { ID: 1, title: "title", tasks: "tasks", date: 1595670780000, link: "null" }
+            add(val.title, val.tasks, val.date, val.ID, val.link);
+        });
+    });
+}
+
+function edit(id) {
+    window.editing = true;
+    let title = document.getElementById('title-' + id);
+    let tasks = document.getElementById('tasks-' + id);
+    let link = document.getElementById('link-' + id);
+    let time = document.getElementById('time-' + id);
+    let edit = document.getElementById('edit-' + id);
+
+    title.innerHTML = `<input id="${title.id}" type="text" value="${title.innerText}">`;
+    tasks.innerHTML = `<input id="${tasks.id}" type="text" value="${tasks.innerText}">`;
+    link.innerHTML = `<input id="${link.id}" type="text" value="${link.innerText}">`;
+    time.innerHTML = `<input id="${time.id}" type="text" value="${time.innerText}">`;
+    edit.innerText = "Save";
+    edit.onclick = function() {
+        save(this.parentElement.parentElement.id);
+    }
+
+}
+
+function save(id) {
+    window.editing = false;
+    let title = document.getElementById('title-' + id);
+    let tasks = document.getElementById('tasks-' + id);
+    let link = document.getElementById('link-' + id);
+    let time = document.getElementById('time-' + id);
+    let edit = document.getElementById('edit-' + id);
+
+    title.innerHTML = `<td id="${title.id}">${title.value}</td>`;
+    tasks.innerHTML = `<td id="${tasks.id}">${tasks.value}</td>`;
+    link.innerHTML = `<td id="${link.id}">${link.value}</td>`;
+    time.innerHTML = `<td id="${time.id}">${time.value}</td>`;
+    edit.innerText = "Edit";
+    edit.onclick = function() {
+        edit(this.parentElement.parentElement.id);
+    }
+
 }
