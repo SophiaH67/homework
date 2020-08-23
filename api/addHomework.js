876c6fcb274e0d4fb65ff15b4690b341836e7bd0
marnixah <marnix@hagehoeve.com>
2020-08-23 10:49:57 +0200

    Use strict in all API scripts

--- api/addHomework.js
+++ api/addHomework.js
@@ -1,4 +1,4 @@
-
+'use strict';
 exports.run = function (req) {
     const connection = require('../lib/sql.js').connection;
     let values = [];
--- api/getHomework.js
+++ api/getHomework.js
@@ -1,4 +1,4 @@
-
+'use strict';
 exports.run = function (req) {
     const connection = require('../lib/sql.js').connection;
     return connection.query("SELECT * FROM entries WHERE date > " + new Date().getTime());
